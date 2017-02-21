2ffec967bfcd62093c4ed6fd2b2dd4dad3b9aa7f
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-02-21 17:09:57 -0600

    add gallery in about

--- wp-content/themes/happytour/template-parts/content-page.php
+++ wp-content/themes/happytour/template-parts/content-page.php
@@ -13,21 +13,10 @@
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php
-			the_content();
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'happytour' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
 	
-		<?php $images = rwmb_meta( 'rw_gallery_thumb', 'type=image&size=large' ); 
+	<?php $images = rwmb_meta( 'rw_gallery_thumb', 'type=image&size=large' ); 
 	         if ( $images ) {?>
-	         <div class="entry-gallery alingright">
+	         <div class="entry-gallery alignright">
 		         <div class="slides">
 
 	             
@@ -47,7 +36,19 @@
 	         <?php         
 	              }
 	          ?>
-	</div>
+	<div class="entry-content">
+		<?php
+			the_content();
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'happytour' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+	
+		
+	
 
 	<?php if ( get_edit_post_link() ) : ?>
 		<footer class="entry-footer">
